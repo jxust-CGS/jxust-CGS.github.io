ce16865156879420c93d5f16fa6c77dac298a78a
Your Name <1055038041@qq.com>
2017-03-11 16:16:08 +0800

    Site updated: 2017-03-11 16:16:00

--- js/scripts.js
+++ js/scripts.js
@@ -82,7 +82,7 @@ $(document).ready(function($) {
 		            var st = $(this).scrollTop();
 		            divs.css({
 		                'margin-top': -(st / 0) + "px",
-		                'opacity': 0.9 - st / 1600
+		                'opacity': 0.6 - st / 1600
 		            });
 		        });
 		    });
